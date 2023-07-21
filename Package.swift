e8553e0742ed9f472dba770032398e4b2e12f4f1
dearhui <dearhui@gmail.com>
2023-07-21 14:16:40 +0800

    remove LSTools files

--- Package.swift
+++ Package.swift
@@ -13,18 +13,15 @@ let package = Package(
             targets: ["MingSwiftUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI", from: "2.0.0"),
-        .package(url: "https://github.com/CodeSlicing/pure-swift-ui", from: "3.0.0"),
-        .package(url: "https://github.com/yeahdongcn/RSBarcodes_Swift", from: "5.1.1")
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MingSwiftUI",
-            dependencies: ["SDWebImageSwiftUI", .product(name: "PureSwiftUI", package: "pure-swift-ui"), "RSBarcodes_Swift"]),
+            name: "MingSwiftUI"),
         .testTarget(
             name: "MingSwiftUITests",
-            dependencies: ["MingSwiftUI", "SDWebImageSwiftUI", .product(name: "PureSwiftUI", package: "pure-swift-ui"), "RSBarcodes_Swift"]),
+            dependencies: ["MingSwiftUI"]),
     ]
 )
--- Tests/MingSwiftUITests/MingSwiftUITests.swift
+++ Tests/MingSwiftUITests/MingSwiftUITests.swift
@@ -3,11 +3,4 @@ import XCTest
 
 final class MingSwiftUITests: XCTestCase {
     
-    // test version conpare
-    func testVersionCompare() throws {
-        let current = "1.1.4"
-        let support = "1.1.3"
-        
-        XCTAssertTrue(current.isVersionSupported(support: support))
-    }
 }
